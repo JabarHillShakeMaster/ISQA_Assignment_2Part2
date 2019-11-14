953ff904fc628562a912e4561a09522c3691e6cc
WinLiniOS32 <majshake80@gmail.com>
2019-11-13 21:15:48 -0600

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\majsh\desktop\3900\Assignment2\foodservice\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
